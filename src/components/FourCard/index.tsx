1c77b91f0b77d276a1471c86130fa596fa6b4a6a
DatGameDev26 <145632083+DatGameDev26@users.noreply.github.com>
2023-09-26 19:15:26 +0700

    Create index.tsx

---
+++ src/components/FourCard/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './index.scss'
+
+
+export function SignupForm() {
+    return (
+        <div className="SignupForm">
+            <h3>Sign Up</h3>
+            <div className="fullName">
+                <input className="name" type="text" placeholder="First name" />
+                <input className="name" type="text" placeholder="Last name" />
+            </div>
+            <input className="email" type="text" placeholder="Email address" />
+            <p>(Check your Gmail for Passcode)</p>
+            <input className="passcode" type="text" placeholder="HJAKA - HFDJK - HDJKS - KIJLM" pattern="[0-9]{3}[0-9]{3}[0-9]{4}"/>
+            <p>By signing up you agree to GraFish’s <a href="">Terms of Service</a>
+            &nbsp; & &nbsp; <a href="">Privacy Policy</a>. This page is protected by reCAPTCHA and is subjected to  Google’s Team</p>
+            <button className="signupButton">Sign Up</button>
+        </div>
+    )
+}
